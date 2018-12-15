c95da6a0d2cac31ef6069fa319d608d138f31fce
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-15 12:29:36 -0800

    Added getting a single expression

---
+++ 3-getting-a-single-expression.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const { getElementById, seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  res.send(foundExpression);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
